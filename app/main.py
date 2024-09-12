e0385b512134bf960bc011590c5de96eb386a8c3
alamin40 <hossain.78295@gmail.com>
2024-09-13 00:12:57 +0200

    adding main file

--- app/main.py
+++ app/main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from fastapi import FastAPI
+from app.routers import tasks, websocket
+from app.database import database, engine, metadata
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+metadata.create_all(engine)
 
+app = FastAPI()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Register routers
+app.include_router(tasks.router)
+app.include_router(websocket.router)
