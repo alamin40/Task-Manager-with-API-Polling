19a8265c5d90952ba38ada5126caa73b50ac9514
alamin40 <hossain.78295@gmail.com>
2024-09-13 00:13:09 +0200

    adding main file

--- app/main.py
+++ app/main.py
@@ -9,3 +9,12 @@ app = FastAPI()
 # Register routers
 app.include_router(tasks.router)
 app.include_router(websocket.router)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
