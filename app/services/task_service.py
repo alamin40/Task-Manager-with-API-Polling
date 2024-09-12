2830b575af08f11b326f37c2fd4713980e441e81
alamin40 <hossain.78295@gmail.com>
2024-09-13 00:09:13 +0200

    adding service code

---
+++ app/services/task_service.py
@@ -0,0 +1,9 @@
+from app.models.task_model import tasks
+from app.database import database
+from app.schemas.task_schema import TaskCreate
+
+async def create_task(task: TaskCreate):
+    query = tasks.insert().values(title=task.title, description=task.description, completed=False)
+    task_id = await database.execute(query)
+    return task_id
+
