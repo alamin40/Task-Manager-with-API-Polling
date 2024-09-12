82d2fe8e99190f3daa55ac7e2da1d33bc9a20001
alamin40 <hossain.78295@gmail.com>
2024-09-13 00:12:33 +0200

    adding web socketcode

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -3,3 +3,17 @@ from typing import List
 
 router = APIRouter()
 connected_clients: List[WebSocket] = []
+
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connected_clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in connected_clients:
+                await client.send_text(f"Message: {data}")
+    except Exception as e:
+        connected_clients.remove(websocket)
+        await websocket.close()
