2168b599dbece6b0c6215b411229b593b14b76bc
alamin40 <hossain.78295@gmail.com>
2024-09-13 00:10:36 +0200

    adding router-task api code

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -3,3 +3,7 @@ from app.schemas.task_schema import Task, TaskCreate
 from app.services import task_service
 
 router = APIRouter()
+
+@router.get("/tasks", response_model=list[Task])
+async def get_tasks():
+    return await task_service.get_all_tasks()
